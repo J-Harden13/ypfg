c3b4d1d6b0483ae4d5513470e0913c58cb7e217d
J-Harden13 <632608937@qq.com>
2019-04-21 21:51:56 +0800

    一片风光

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function () {
+    var big_height = $("#index").position().top();
+    var pdt_introduce_height = parseInt($("#pdt_introduce").position().top()-231);
+    var pdt_ctt_height = parseInt($("#pdt_ctt").position().top()-231);
+    var apt_scene_height = parseInt($("#apt_scene").position().top()-231);
+    var pht_factory_height = parseInt($("#pht_factory").position().top()-231);
+    var enp_int_height = parseInt($("#enp_int").position().top()-231);
+    var otr_height = parseInt($("#otr").position().top()-231);
+    var me_footer_height = parseInt($("me_footer").position.top()-231);
+    var top = parseInt($(this).scrollTop());
+})
